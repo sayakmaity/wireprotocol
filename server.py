52107af38bc884caadd979b5ca83ae52b61e5562
Sayak Maity <smaity@college.harvard.edu>
2023-02-15 17:53:52 -0500

    server should be working, user certainly not working atm

--- server.py
+++ server.py
@@ -1,189 +1,85 @@
-import socket
-import threading
-from codes import Requests, Responses
-import logging
-import fnmatch
-from base_server import BaseServer
+import grpc
+import time
+import chat_pb2
+import chat_pb2_grpc
+import concurrent
 
-from user import User
+# Define the chat service class
+class ChatService(chat_pb2_grpc.ChatServiceServicer):
+    # Define a dictionary to store the list of users and their channels
+    users = {}
 
-class Server(BaseServer):
-    """
-    A class representing a server that can handle multiple clients using threads.
+    def Login(self, request, context):
+        # Check if the user is already logged in
+        if request.username in self.users:
+            return chat_pb2.LoginResponse(success=False, message="User already logged in")
 
-    Attributes:
-        host (str): The IP address of the server host.
-        port (int): The port number to use for the server.
-        encoding (str): The encoding format to use for the messages.
-        header_length (int): The header size of the message in bytes.
-        server (socket.socket): The server socket instance.
+        # Add the user to the list of users and create a new channel for them
+        self.users[request.username] = context
+        return chat_pb2.LoginResponse(success=True, message="User logged in")
 
-    Parameters:
-        host (str, optional): The IP address of the server host. Defaults to the local machine's IP address.
-        port (int, optional): The port number to use for the server. Defaults to 5050.
-        encoding (str, optional): The encoding format to use for the messages. Defaults to 'utf-8'.
-        header_length (int, optional): The header size of the message in bytes. Defaults to 64.
-    """
-    def __init__(self,
-        host: str = socket.gethostbyname(socket.gethostname()),
-        port: int = 5050,
-        encoding: str = 'utf-8',
-        header_length: int = 64,
-    ):
-        super().__init__(host, port, encoding, header_length)
-        
-        self.clients_lock = threading.Lock()
-        self.clients = []
+    def Logout(self, request, context):
+        # Check if the user is logged in
+        if request.username not in self.users:
+            return chat_pb2.LoginResponse(success=False, message="User not logged in")
 
-        self.users_lock = threading.Lock()
-        self.usernames = []
-        self.username_to_user = {}
-        self.active_connections = {}
+        # Remove the user from the list of users and close their channel
+        del self.users[request.username]
+        context.cancel()
+        return chat_pb2.LoginResponse(success=True, message="User logged out")
 
-        self.requests = {
-            Requests.LOGIN: self.handle_login,
-            Requests.CREATE_ACCOUNT: self.handle_create_account,
-            Requests.DELETE_ACCOUNT: self.handle_delete_account,
-            Requests.LIST_ACCOUNTS: self.handle_list_accounts,
-            Requests.SEND_MESSAGE: self.handle_send_message,
-            Requests.VIEW_MESSAGES: self.handle_view_messages,
-            Requests.DISCONNECT: self.disconnect
-        }
+    def CreateAccount(self, request, context):
+        # Check if the username already exists
+        if request.username in self.users:
+            return chat_pb2.CreateAccountResponse(success=False, message="Username already taken")
 
-        # Create a new server socket instance and bind it to the specified host and port
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server.bind((self.host, self.port))
+        # Add the user to the list of users and create a new channel for them
+        self.users[request.username] = context
+        return chat_pb2.CreateAccountResponse(success=True, message="Account created")
 
-    def handle_login(self, conn, msg):
-        username = msg
-        with self.users_lock:
-            if username in self.usernames:
-                if username in self.active_connections:
-                    return self.generate_payload(Responses.FAILURE, True, "User already logged in")
-                else:
-                    self.active_connections[username] = conn
-                    return self.generate_payload(Responses.SUCCESS, True, "User logged in")
-            else:
-                return self.generate_payload(Responses.FAILURE, True, "Username does not exist")
+    def DeleteAccount(self, request, context):
+        # Check if the user is logged in
+        if request.username not in self.users:
+            return chat_pb2.LoginResponse(success=False, message="User not logged in")
 
-    def handle_create_account(self, conn, msg):
-        username = msg
-        with self.users_lock:
-            if username in self.usernames:
-                return self.generate_payload(Responses.FAILURE, True, "Username already exists")
-            else:
-                new_user = User(username)
-                self.usernames.append(username)
-                self.username_to_user[username] = new_user
-                return self.generate_payload(Responses.SUCCESS, True, "User Created")
+        # Remove the user from the list of users and close their channel
+        del self.users[request.username]
+        context.cancel()
+        return chat_pb2.DeleteAccountResponse(success=True, message="Account deleted")
 
+    def ListAccounts(self, request, context):
+        # Create a list of usernames matching the pattern
+        usernames = [username for username in self.users.keys() if request.pattern in username]
+        return chat_pb2.ListAccountsResponse(usernames=usernames)
 
-    def handle_delete_account(self, conn, msg):
-        username = msg
-        with self.users_lock:
-            if username in self.usernames:
-                user = self.username_to_user[username]
-                del user
-                self.usernames.remove(username)
-                del self.username_to_user[username]
-                return self.generate_payload(Responses.SUCCESS, True, "Account deleted successfully")
-            else:
-                return self.generate_payload(Responses.FAILURE, True, "Account not found")
+    def SendMessage(self, request, context):
+        # Check if the sender is logged in
+        if request.sender not in self.users:
+            return chat_pb2.SendMessageResponse(success=False, message="Sender not logged in")
 
-    def handle_list_accounts(self, conn, msg):
-        """
-        Handle a list accounts request from a client.
-        The method returns a list of all usernames that match the provided regex-like query.
+        # Check if the receiver is logged in
+        if request.receiver not in self.users:
+            return chat_pb2.SendMessageResponse(success=False, message="Receiver not logged in")
 
-        Parameters:
-        conn (socket.socket): The client socket connection.
-        msg (str): The query to search for.
+        # Send the message to the receiver
+        receiver_context = self.users[request.receiver]
+        receiver_context.send_initial_metadata(('sender', request.sender),)
+        receiver_context.send_message(request)
 
-        Returns:
-        dict: The response metadata in the form of a dictionary.
-        """
-        query = msg.strip()
-        matching_accounts = []
-        for username in self.usernames:
-            if fnmatch.fnmatch(username, query):
-                matching_accounts.append(username)
-        if matching_accounts:
-            response_message = "\n".join(matching_accounts)
-            return self.generate_payload(Responses.SUCCESS, True, f"\n{response_message}")
-        else:
-            return self.generate_payload(Responses.FAILURE, True, "No matching accounts found.")
+        return chat_pb2.SendMessageResponse(success=True, message="Message sent")
 
+# Create a gRPC server and add the chat service to it
+server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=10))
+chat_pb2_grpc.add_ChatServiceServicer_to_server(ChatService(), server)
 
-    def handle_send_message(self, conn, msg):
-        sender, receiver, text_message = msg.split("\n")
-        receiver_conn = None
-        with self.clients_lock:
-            for username in self.usernames:
-                if username == receiver:
-                    receiver_conn = self.active_connections.get(username)
-                    break
-        if receiver_conn:
-            msg = f"{sender}\n{text_message}"
+# Start the server
+print("Starting server...")
+server.add_insecure_port('[::]:50051')
+server.start()
 
-            message = msg.encode(self.encoding)
-            msg_len = len(message)
-            send_length = str(msg_len).encode(self.encoding)
-            send_length += b' ' * (self.header_length - len(send_length))
-
-            receiver_conn.send(send_length)
-            receiver_conn.send(message)
-
-
-            # self.send_message(receiver_conn, Responses.SUCCESS, f"{sender}\n{text_message}")
-            return self.generate_payload(Responses.SUCCESS, True, "Message sent.")
-        else:
-            return self.generate_payload(Responses.FAILURE, True, "Receiver not found.")
-
-
-    def handle_view_messages(self, conn, msg):
-        # TODO: handle view messages request
-        pass
-
-    def disconnect(self, conn, msg=""):
-        with self.clients_lock:
-            index = self.clients.index(conn)
-            conn.close()
-            self.clients.pop(index)
-
-        with self.users_lock:
-            print(self.active_connections)
-            if conn in self.active_connections.values():
-                for k in self.active_connections.copy():
-                    if self.active_connections[k] == conn:
-                        del self.active_connections[k]
-            print(self.active_connections)
-        return self.generate_payload(Responses.SUCCESS, False, "Disconnected!")
-    
-
-    def start(self):
-        """
-        Start the server and listen for incoming connections.
-        """
-        self.start_logger()
-        self.server.listen()
-        logging.info(f"[LISTENING] Server is listening on port {self.port}")
-        try:
-            while True:
-                conn, addr = self.server.accept()
-                with self.clients_lock:
-                    self.clients.append(conn)
-                thread = threading.Thread(target=self.handle_client, args=(conn, addr))
-                thread.start()
-                logging.info(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
-        except KeyboardInterrupt:
-            logging.info("[SHUTTING DOWN] Closing server socket...")
-            self.server.close()
-            with self.clients_lock:
-                for conn in self.clients:
-                    self.disconnect(conn)
-            logging.info("[SHUTDOWN COMPLETE] Goodbye!")
-
-if __name__ == "__main__":
-    server = Server()
-    server.start()
+# Keep the server running
+try:
+    while True:
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
--- user.py
+++ user.py
@@ -1,17 +1,105 @@
-from queue import Queue
+import grpc
 import threading
+import chat_pb2
+import chat_pb2_grpc
 
-class User:
-    def __init__(self, username):
-        self.username = username
-        self.message_queue = Queue() # Queue is already thread-safe, don't need to worry if we use default methods
-        self.lock = threading.Lock()
+# Define the chat client class
+class ChatClient:
+    def __init__(self):
+        # Connect to the chat server
+        self.channel = grpc.insecure_channel('localhost:50051')
+        self.stub = chat_pb2_grpc.ChatServiceStub(self.channel)
 
-    def add_message(self, message):
-        self.message_queue.put(message)
+        # Start a thread to receive messages asynchronously
+        self.thread = threading.Thread(target=self.receive_messages)
+        self.thread.daemon = True
+        self.thread.start()
 
-    def get_message(self):
-        if self.message_queue.empty():
-            return None
-        return self.message_queue.get()
+    def receive_messages(self):
+        # Continuously receive messages from the server
+        response_iterator = self.stub.SendMessage(chat_pb2.Message(sender='', receiver='', text=''), timeout=None)
+        for message in response_iterator:
+            if hasattr(message, 'sender'):
+                print(f"{message.sender}: {message.text}")
 
+
+    def login(self, username):
+        # Call the Login RPC and return the response
+        return self.stub.Login(chat_pb2.User(username=username))
+
+    def logout(self, username):
+        # Call the Logout RPC and return the response
+        return self.stub.Logout(chat_pb2.User(username=username))
+
+    def create_account(self, username):
+        # Call the CreateAccount RPC and return the response
+        return self.stub.CreateAccount(chat_pb2.User(username=username))
+
+    def delete_account(self, username):
+        # Call the DeleteAccount RPC and return the response
+        return self.stub.DeleteAccount(chat_pb2.User(username=username))
+
+    def list_accounts(self, pattern):
+        # Call the ListAccounts RPC and print the response
+        response = self.stub.ListAccounts(chat_pb2.ListAccountsRequest(pattern=pattern))
+        print("Users:")
+        for username in response.usernames:
+            print(f"- {username}")
+
+    def send_message(self, sender, receiver, text):
+        # Call the SendMessage RPC and return the response
+        return self.stub.SendMessage(chat_pb2.Message(sender=sender, receiver=receiver, text=text))
+
+# Create a chat client
+client = ChatClient()
+
+# Loop to receive commands from the user
+while True:
+    # Receive a command from the user
+    command = input("Enter a command (login/logout/create/delete/list/send/quit): ")
+
+    # Parse the command
+    if command == "login":
+        username = input("Enter your username: ")
+        response = client.login(username)
+        if response.success:
+            print("Logged in")
+        else:
+            print(f"Login failed: {response.message}")
+    elif command == "logout":
+        username = input("Enter your username: ")
+        response = client.logout(username)
+        if response.success:
+            print("Logged out")
+        else:
+            print(f"Logout failed: {response.message}")
+    elif command == "create":
+        username = input("Enter a new username: ")
+        response = client.create_account(username)
+        if response.success:
+            print("Account created")
+        else:
+            print(f"Account creation failed: {response.message}")
+    elif command == "delete":
+        username = input("Enter the username to delete: ")
+        response = client.delete_account(username)
+        if response.success:
+            print("Account deleted")
+        else:
+            print(f"Account deletion failed: {response.message}")
+    elif command == "list":
+        pattern = input("Enter a username pattern: ")
+        client.list_accounts(pattern)
+    elif command == "send":
+        sender = input("Enter your username: ")
+        receiver = input("Enter the receiver's username: ")
+        text = input("Enter the message: ")
+        response = client.send_message(sender, receiver, text)
+        if response.success:
+            print("Message sent")
+        else:
+            print(f"Message sending failed: {response.message}")
+    elif command == "quit":
+        break
+    else:
+        print("Invalid command")
